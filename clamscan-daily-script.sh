0b6b3508600a6ea0be6bdc42ab73324ec83fc50b
crylium <tomas@lisenet.com>
2020-04-06 11:59:04 +0100

    ShellCheck friendly

--- clamscan-daily-script.sh
+++ clamscan-daily-script.sh
@@ -6,7 +6,7 @@
 LOGFILE="/var/log/clamav/clamav-$(date +'%Y-%m-%d').log";
 HOST="$(hostname --long)";
 EMAIL_MSG="Please see the log file attached.";
-EMAIL_FROM="clamav-daily@"$HOST"";
+EMAIL_FROM="clamav-daily@${HOST}";
 EMAIL_TO="admin@example.com";
 DIRTOSCAN="/home";
 
@@ -22,19 +22,19 @@ TODAY=$(date +%u);
 if [ "$TODAY" == "6" ];then
 	echo "Starting a full weekend scan.";
 	# be nice to others while scanning the entire root
-	nice -n5 clamscan -ri / --exclude-dir=/sys/ &>"$LOGFILE";
+	nice -n5 clamscan -ri / --exclude-dir=/sys/ --exclude-dir=/proc/ &>"${LOGFILE}";
 else
-	DIRSIZE=$(du -sh "$DIRTOSCAN"  2>/dev/null|cut -f1);
-	echo -e "Starting a daily scan of "$DIRTOSCAN" directory.\nAmount of data to be scanned is "$DIRSIZE".";
-	clamscan -ri "$DIRTOSCAN" &>"$LOGFILE";
+	DIRSIZE=$(du -sh "${DIRTOSCAN}" 2>/dev/null|cut -f1);
+	echo -e "Starting a daily scan of ${DIRTOSCAN} directory.\nAmount of data to be scanned is ${DIRSIZE}.";
+	clamscan -ri "${DIRTOSCAN}" &>"${LOGFILE}";
 fi
 
 # get the value of "Infected lines"
-MALWARE=$(tail "$LOGFILE"|grep Infected|cut -d" " -f3);
+MALWARE=$(tail "${LOGFILE}"|grep Infected|cut -d" " -f3);
 
 # if the value is not equal to zero, send an email with the log file attached
-if [ "$MALWARE" -ne "0" ]; then
-	echo "$EMAIL_MSG"|mail -a "$LOGFILE" -s "ClamAV: Malware Found" -r "$EMAIL_FROM" "$EMAIL_TO";
+if [ "${MALWARE}" -ne "0" ]; then
+	echo "${EMAIL_MSG}"|mail -a "${LOGFILE}" -s "ClamAV: Malware Found" -r "${EMAIL_FROM}" "${EMAIL_TO}";
 fi
 
 echo "The script has finished.";
